fcc55d8cd8953bfac1b9d58a1611fdce7e891645
Marcos Sassali <misassali@gmail.com>
2024-03-05 11:27:06 -0300

    section 3.23 y 3.24

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
-const products = [
-  {name: 'product1', price: 100.00},
-  {name: 'product2', price: 200.00},
-  {name: 'product2', price: 200.00},
-]
+import { useEffect, useState } from "react"
 
 function App() {
+  const [products, setProducts] = useState([
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00},
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products/')
+    .then(Response => Response.json())
+    .then(data => setProducts(data))
+  }, [])
+
+  function addProduct(){
+    setProducts(prevState => [...prevState, {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100.00}])
+  }
+
   return (
     <div>
      <h1>Re-Store</h1>
@@ -13,6 +24,7 @@ function App() {
         <li key={index}>{item.name} - {item.price}</li>
       ))}
      </ul>
+     <button onClick={addProduct}>Add Product</button>
     </div>
   )
 }
