dffc72c55fde1f45f971b16680e85b0896c6c02e
Marcos Sassali <misassali@gmail.com>
2024-03-05 14:38:26 -0300

    Section 3.29

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,36 +1,32 @@
 import { useEffect, useState } from "react"
 import { Product } from "../models/products";
+import Catalog from "../../features/catalog/Catalog";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:5000/api/products/')
-    .then(Response => Response.json())
-    .then(data => setProducts(data))
+      .then(Response => Response.json())
+      .then(data => setProducts(data))
   }, [])
 
-  function addProduct(){
-    setProducts(prevState => [...prevState, 
-      {
-        id: prevState.length + 101,
-        name: 'product' + (prevState.length + 1),
-        price: (prevState.length * 100) + 100.00,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200'
-      }])
+  function addProduct() {
+    setProducts(prevState => [...prevState,
+    {
+      id: prevState.length + 101,
+      name: 'product' + (prevState.length + 1),
+      price: (prevState.length * 100) + 100.00,
+      brand: 'some brand',
+      description: 'some description',
+      pictureUrl: 'http://picsum.photos/200'
+    }])
   }
 
   return (
     <div>
-     <h1>Re-Store</h1>
-     <ul>
-      {products.map(product =>(
-        <li key={product.id}>{product.name} - {product.price}</li>
-      ))}
-     </ul>
-     <button onClick={addProduct}>Add Product</button>
+      <h1>Re-Store</h1>
+      <Catalog products={products} addProduct={addProduct} />
     </div>
   )
 }
